84553d472965a2175e5f62c7e7256d59553ca42e
Nolan Pederencino <npederen@c1r3p3.42perpignan.fr>
2025-05-03 18:44:43 +0200

    readline is looping

--- main.c
+++ main.c
@@ -6,19 +6,23 @@
 /*   By: npederen <npederen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/03 15:23:42 by npederen          #+#    #+#             */
-/*   Updated: 2025/05/03 15:42:47 by npederen         ###   ########.fr       */
+/*   Updated: 2025/05/03 18:44:26 by npederen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "minishell.h"
 
-#include <stdio.h>
-#include <readline/readline.h>
-#include <readline/history.h>
-
-int main(void)
+int	main(void)
 {
-    char *rl;
-    rl = readline("Minishell$ ");
-    printf("%s\n", rl);
-    return (0);
+	char	*rl;
+
+	while (1)
+	{
+		rl = readline("Minishell$ ");
+		printf("%s\n", rl);
+		free(rl);
+		if (rl == NULL)
+			break ;
+	}
+	return (0);
 }
