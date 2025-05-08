cf402d4d883212538d6a9710dcab2d40d00f60b5
Zephyre <laurelineduf@proton.me>
2025-05-08 12:03:17 +0200

    Add file readline.supp = remove the still reachable memory define by readline, add command_exit for quit proprely during the testing program

--- main.c
+++ main.c
@@ -6,11 +6,28 @@
 /*   By: npederen <npederen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/03 15:23:42 by npederen          #+#    #+#             */
-/*   Updated: 2025/05/08 02:18:25 by npederen         ###   ########.fr       */
+/*   Updated: 2025/05/08 12:00:40 by lduflot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+/*
+	* Add command exit for quit programm properly during the test ./minishell
+*/
+void	command_exit_for_testing(char *line, t_token *token)
+{
+	int	size_line;
+
+	size_line = ft_strlen(line) + 1;
+	if (line && ft_strncmp(line, "exit", size_line) == 0)
+	{
+		free_token(token);
+		free(line);
+		rl_clear_history();
+		exit(0);
+	}
+}
 
 //Readline leak ==296785==    still reachable: 214,430 bytes in 259 blocks
 int	main(void)
@@ -24,12 +41,13 @@ int	main(void)
 		line = readline("Minishell$ ");
 		if (line == NULL)
 			break ;
+		command_exit_for_testing(line, token);
 		token = tokenize(token, &line);
 		add_history(line);
 		print_token_list(token);
 		free_token(token);
 		free(line);
-		rl_clear_history();
 	}
+	rl_clear_history();
 	return (0);
 }
