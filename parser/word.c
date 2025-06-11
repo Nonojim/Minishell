312960310e84f27f70ef8b723c15f13d3c54843f
Zephyre <laurelineduf@proton.me>
2025-06-11 10:41:41 +0200

    add function parsing_word

--- parser/word.c
+++ parser/word.c
@@ -6,7 +6,7 @@
 /*   By: npederen <npederen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/28 11:30:57 by lduflot           #+#    #+#             */
-/*   Updated: 2025/06/11 10:17:59 by lduflot          ###   ########.fr       */
+/*   Updated: 2025/06/11 10:41:16 by lduflot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,14 @@
 
 t_treenode	*parse_word_node(t_token **token_list);
 t_treenode	*parse_word1(t_token **token_list);
-int	parse_cmd(char *cmd);
+int	parse_cmd(char *cmd, int *error);
 
 t_treenode *parse_word_node(t_token **tokens)
 {
 	int	argc = 0;
 	int	i = 0;
 	t_token *tmp = *tokens;
+	static int	error = 0;
 
 	//printf("%p\n", (*tokens)->str);
 	while (tmp != NULL
@@ -52,9 +53,8 @@ t_treenode *parse_word_node(t_token **tokens)
 	//printf("%p\n", (*tokens)->str);
 	t_treenode *cmd = create_treenode(0, argv[0]);
 	cmd->argv = argv;
-	if (parse_cmd(argv[0]) == 1)
+	if (parse_cmd(argv[0], &error) == 1)
 	{
-		printf("%s: command not found\n", argv[0]);
 		free_treenode(cmd);
 		return (NULL);
 	}
@@ -62,7 +62,10 @@ t_treenode *parse_word_node(t_token **tokens)
 	return cmd;
 }
 
-int	parse_cmd(char *cmd)
+//ne fonctionne plus au second prompt, il faut réniatialiser la variable statique
+// il faut penser que quand on met un " || " l'arbre doit continuer à se crée pour la suite du || !!!!!!
+// cette fonction ne fonctionnera pas quand il y a une redirection > txt etc renverra une erreur
+int	parse_cmd(char *cmd, int *error)
 {
 	if (ft_strcmp(cmd, "echo") == 0)
 		return (0);
@@ -78,6 +81,11 @@ int	parse_cmd(char *cmd)
 		return (0);
 	else if (ft_strcmp(cmd, "exit") == 0)
 		return (0);
-	else
+	else if (*error == 0)
+	{
+		printf("%s: command not found\n", cmd);
+		*error = 1;
 		return (1);
+	}
+	return (1);
 }
