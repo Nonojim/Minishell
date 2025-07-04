6d70a4ad7d321d55417c6c6071d97ca768da111a
nonojim <nolan.pederencino@gmail.com>
2025-07-04 14:01:17 +0200

    env in the right order by recursivity

--- execution/builtins/builtins.h
+++ execution/builtins/builtins.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   builtins.h                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lduflot <lduflot@student.42perpignan.fr>   +#+  +:+       +#+        */
+/*   By: npederen <npederen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/24 19:05:07 by lduflot           #+#    #+#             */
-/*   Updated: 2025/06/30 11:59:48 by lduflot          ###   ########.fr       */
+/*   Updated: 2025/07/04 12:57:21 by npederen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,26 +16,27 @@
 # include "../../minishell.h"
 
 // cd
-int		ft_cd(t_treenode *node);
+int			ft_cd(t_treenode *node);
 // echo
-int		ft_echo(t_treenode *node);
-int		ft_echo_n(char *argv);
+int			ft_echo(t_treenode *node);
+int			ft_echo_n(char *argv);
 // env
-int		ft_env(t_env *env_list);
+int			ft_env(t_env *env_list);
+static void	print_env_reverse(t_env *node);
 //unset
-int		ft_unset(t_treenode *node);
-int		unset_head_list(t_treenode *node, char *key);
-void	unset_list(t_treenode *node, char *key);
+int			ft_unset(t_treenode *node);
+int			unset_head_list(t_treenode *node, char *key);
+void		unset_list(t_treenode *node, char *key);
 // exit
-int		compar_long_limits(char *nb, char sign);
-int		is_numeric_exit(char *argv);
-void	error_numeric_exit(t_token *token, char *line, t_treenode *ast);
-int		ft_exit(char *line, t_token *token, t_treenode *ast);
+int			compar_long_limits(char *nb, char sign);
+int			is_numeric_exit(char *argv);
+void		error_numeric_exit(t_token *token, char *line, t_treenode *ast);
+int			ft_exit(char *line, t_token *token, t_treenode *ast);
 // export
-int		ft_export(t_treenode *node);
-void	print_export(t_env *env);
-void	add_export_variable(t_treenode *node, char *arg);
+int			ft_export(t_treenode *node);
+void		print_export(t_env *env);
+void		add_export_variable(t_treenode *node, char *arg);
 // pwd
-int		ft_pwd(t_treenode *node);
+int			ft_pwd(t_treenode *node);
 
 #endif
--- execution/builtins/env.c
+++ execution/builtins/env.c
@@ -6,22 +6,36 @@
 /*   By: npederen <npederen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/21 17:49:03 by npederen          #+#    #+#             */
-/*   Updated: 2025/06/30 11:57:46 by lduflot          ###   ########.fr       */
+/*   Updated: 2025/07/04 12:55:37 by npederen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "builtins.h"
 
+//int	ft_env(t_env *env_list)
+//{
+//	t_env	*tmp;
+//
+//	tmp = env_list;
+//	while (tmp)
+//	{
+//		if (tmp->value)
+//			printf("%s=%s\n", tmp->key, tmp->value);
+//		tmp = tmp->next;
+//	}
+//	return (0);
+//}
+static void	print_env_reverse(t_env *node)
+{
+	if (!node)
+		return;
+	print_env_reverse(node->next);
+	if (node->value)
+		printf("%s=%s\n", node->key, node->value);
+}
+
 int	ft_env(t_env *env_list)
 {
-	t_env	*tmp;
-
-	tmp = env_list;
-	while (tmp)
-	{
-		if (tmp->value)
-			printf("%s=%s\n", tmp->key, tmp->value);
-		tmp = tmp->next;
-	}
+	print_env_reverse_rec(env_list);
 	return (0);
 }
