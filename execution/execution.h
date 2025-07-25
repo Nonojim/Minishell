e2a6d5816baded6fee29f46dbb8e315c8ca0148e
Nonojim <nolan.pederencino@gmail.com>
2025-07-25 19:12:54 +0200

    norm in header

--- execution/execution.h
+++ execution/execution.h
@@ -6,7 +6,7 @@
 /*   By: npederen <npederen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/20 22:14:13 by npederen          #+#    #+#             */
-/*   Updated: 2025/07/24 17:15:18 by lduflot          ###   ########.fr       */
+/*   Updated: 2025/07/25 19:10:55 by npederen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,64 +31,66 @@ typedef struct s_ctx
 }	t_ctx;
 
 //list to char**
-char	**list_to_dynamiccarray(t_ctx *ctx);
+char		**list_to_dynamiccarray(t_ctx *ctx);
 
 // Exe
-int		execute_node(t_treenode *node, char *line, t_ctx *ctx);
-int		execute_node_simple(t_treenode *node, char *line, t_ctx *ctx);
-int		execute_node_logical(t_treenode *node, char *line, t_ctx *ctx);
-int		execute_node_redir(t_treenode *node, char *line, t_ctx *ctx);
-void	execute_tree(t_treenode *tree, char *line, t_ctx *ctx);
+int			execute_node(t_treenode *node, char *line, t_ctx *ctx);
+int			execute_node_simple(t_treenode *node, char *line, t_ctx *ctx);
+int			execute_node_logical(t_treenode *node, char *line, t_ctx *ctx);
+int			execute_node_redir(t_treenode *node, char *line, t_ctx *ctx);
+void		execute_tree(t_treenode *tree, char *line, t_ctx *ctx);
 
 // Exe_Simple_Command
-int		is_builtin(char *cmd);
-int		execute_simple_command(t_treenode *node, char *line, t_ctx *ctx);
-int		execute_external_command(t_treenode *node, t_ctx *ctx, char *line);
-int		execute_builtin_command(t_treenode *node, char *line, t_ctx *ctx);
-int		external_command_status(t_ctx *ctx, pid_t pid);
-char	*find_cmd_path(char *cmd, t_env *env_list);
-void	free_split(char **split);
-char	**list_to_dynamiccarray(t_ctx *ctx);
-void	add_value(t_env *tmp, char *key_eq, char **array, int *i);
-int		count_env(t_env *env);
-void	free_execve(t_treenode *node, char *line, t_ctx *ctx, char *cmd_path);
-void	handle_child_process(t_treenode *node, t_ctx *ctx, \
-														char *line, char *cmd);
-char	*shearch_cmd_path(char *cmd, t_ctx *ctx, char *line, t_treenode *node);
+int			is_builtin(char *cmd);
+int			execute_simple_command(t_treenode *node, char *line, t_ctx *ctx);
+int			execute_external_command(t_treenode *node, t_ctx *ctx, char *line);
+int			execute_builtin_command(t_treenode *node, char *line, t_ctx *ctx);
+int			external_command_status(t_ctx *ctx, pid_t pid);
+char		*find_cmd_path(char *cmd, t_env *env_list);
+void		free_split(char **split);
+char		**list_to_dynamiccarray(t_ctx *ctx);
+void		add_value(t_env *tmp, char *key_eq, char **array, int *i);
+int			count_env(t_env *env);
+void		free_execve(t_treenode *node, char *line, t_ctx *ctx,\
+	char *cmd_path);
+void		handle_child_process(t_treenode *node, t_ctx *ctx,\
+	char *line, char *cmd);
+char		*shearch_cmd_path(char *cmd, t_ctx *ctx,\
+	char *line, t_treenode *node);
 
 // Exe_Pipe
-int		execute_pipeline(t_treenode *node, char *line, t_ctx *ctx);
-int		pipe_status(pid_t pid1, pid_t pid2, t_ctx *ctx);
-void	pipe_left(t_treenode *node, char *line, int pipefd[2], t_ctx *ctx);
-void	pipe_right(t_treenode *node, char *line, int pipefd[2], t_ctx *ctx);
+int			execute_pipeline(t_treenode *node, char *line, t_ctx *ctx);
+int			pipe_status(pid_t pid1, pid_t pid2, t_ctx *ctx);
+void		pipe_left(t_treenode *node, char *line, int pipefd[2], t_ctx *ctx);
+void		pipe_right(t_treenode *node, char *line, int pipefd[2], t_ctx *ctx);
 
 // Exe_Subshell
-int		execute_subshell_node(t_treenode *node, char *line, t_ctx *ctx);
-int		subshell_status(t_ctx *ctx, pid_t pid);
+int			execute_subshell_node(t_treenode *node, char *line, t_ctx *ctx);
+int			subshell_status(t_ctx *ctx, pid_t pid);
 
 // Exe_HereDoc
-int		execute_heredoc_node(t_treenode *node, char *line, t_ctx *ctx);
-int		heredoc_status(t_ctx *ctx, pid_t pid, t_treenode *node, char *line);
+int			execute_heredoc_node(t_treenode *node, char *line, t_ctx *ctx);
+int			heredoc_status(t_ctx *ctx, pid_t pid, t_treenode *node, char *line);
 
 // Exe_Redirection
-int		execute_redirection_chain(t_treenode *node, char *line, t_ctx *ctx);
-int		redir_input(t_treenode *node, t_ctx *ctx);
-int		redir_output(t_treenode *node, t_ctx *ctx);
-int		redir_append(t_treenode *node, t_ctx *ctx);
-int		apply_redirections(t_treenode *node, char *line, t_ctx *ctx);
-void	dup_and_close(int saved_stdin, int saved_stdout);
-void	new_argv_cmd(t_treenode *redir_node);
-char	**create_new_array_cmd(t_treenode *redir_node, t_treenode *cmd);
-void	create_new_argv_cmd(char **new_argv, int *i, t_treenode *node);
+int			execute_redirection_chain(t_treenode *node, char *line, t_ctx *ctx);
+int			redir_input(t_treenode *node, t_ctx *ctx);
+int			redir_output(t_treenode *node, t_ctx *ctx);
+int			redir_append(t_treenode *node, t_ctx *ctx);
+int			apply_redirections(t_treenode *node, char *line, t_ctx *ctx);
+void		dup_and_close(int saved_stdin, int saved_stdout);
+void		new_argv_cmd(t_treenode *redir_node);
+char		**create_new_array_cmd(t_treenode *redir_node, t_treenode *cmd);
+void		create_new_argv_cmd(char **new_argv, int *i, t_treenode *node);
 t_treenode	*find_command_node(t_treenode *node);
 
 // Env
-t_env	*init_env_list(void);
-void	change_shlvl(t_env	**env);
-void	export_to_env(t_env **env_list, char *key, char *value);
-t_env	*find_usrvar(t_env *env, const char *key);
-void	free_env_list(t_env *env);
-void	add_new_var(t_env **env_list, char *key, char *value);
-void	create_env_list_with_environ(t_env **env_list);
+t_env		*init_env_list(void);
+void		change_shlvl(t_env	**env);
+void		export_to_env(t_env **env_list, char *key, char *value);
+t_env		*find_usrvar(t_env *env, const char *key);
+void		free_env_list(t_env *env);
+void		add_new_var(t_env **env_list, char *key, char *value);
+void		create_env_list_with_environ(t_env **env_list);
 
 #endif
--- expansion/expansion.h
+++ expansion/expansion.h
@@ -6,7 +6,7 @@
 /*   By: npederen <npederen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/17 15:05:55 by lduflot           #+#    #+#             */
-/*   Updated: 2025/07/25 18:58:19 by lduflot          ###   ########.fr       */
+/*   Updated: 2025/07/25 19:12:11 by npederen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,57 +32,57 @@ typedef struct s_wildcard
 }	t_wildcard;
 
 //expansion
-void	expanse_ast(t_treenode *node, t_ctx *ctx);
-void	expanse_argv(t_treenode *node, t_ctx *ctx);
-void	expanse_redir(t_treenode *node, t_ctx *ctx);
-void	expanse_heredoc(t_treenode *node, t_ctx *ctx);
-void	free_redir(t_treenode *node, char *expanded);
+void		expanse_ast(t_treenode *node, t_ctx *ctx);
+void		expanse_argv(t_treenode *node, t_ctx *ctx);
+void		expanse_redir(t_treenode *node, t_ctx *ctx);
+void		expanse_heredoc(t_treenode *node, t_ctx *ctx);
+void		free_redir(t_treenode *node, char *expanded);
 
 //expansion2
-char	*expand_tilde(char *str, t_ctx *ctx);
-char	*expand_string(char *str, t_treenode *node, t_ctx *ctx, \
-			int wildcard_index);
-char	*shearch_and_add_var(char *str, char *result, t_ctx *ctx);
-char	*expand_heredoc(char	*str, t_ctx *ctx);
-int		expand_variable(char *str, int i, char **result, t_ctx *ctx);
+char		*expand_tilde(char *str, t_ctx *ctx);
+char		*expand_string(char *str, t_treenode *node, t_ctx *ctx,\
+	int wildcard_index);
+char		*shearch_and_add_var(char *str, char *result, t_ctx *ctx);
+char		*expand_heredoc(char	*str, t_ctx *ctx);
+int			expand_variable(char *str, int i, char **result, t_ctx *ctx);
 
 //expansion_utils
-int		toggle_quote(char *str, int *i, t_quote_state *q, char **result);
-char	*remove_quotes_after_expansion(const char *str);
-void	add_char_to_string(char **result, char c);
-char	*ft_getenv(char *name, t_ctx *ctx);
+int			toggle_quote(char *str, int *i, t_quote_state *q, char **result);
+char		*remove_quotes_after_expansion(const char *str);
+void		add_char_to_string(char **result, char c);
+char		*ft_getenv(char *name, t_ctx *ctx);
 
 //expanse_wildcard
 
 // wildcard_psm
 t_wildcard	*init_struct_psm(int wildcard_index);
-int		match_prefix(char *str, char *prefix);
-int		match_middle(char *str, char **middle);
-int		match_suffix(char *str, char *suffix);
-char	**add_array(char **result, char *file);
-void	create_prefix_middle_suffix(char *str, t_wildcard *psm);
-int		create_prefix(char *str, t_wildcard *psm);
-void	allocation_psm_middle(t_wildcard *psm, int middle_wildcard);
-void	create_psm_middle_suffix(char *str, t_wildcard *psm, int i);
-int		count_middle_wildcard(char *str);
-void	sort_argv(char **array);
-void	free_expand_variable(char *tmp, char *expanse, char *new_str);
+int			match_prefix(char *str, char *prefix);
+int			match_middle(char *str, char **middle);
+int			match_suffix(char *str, char *suffix);
+char		**add_array(char **result, char *file);
+void		create_prefix_middle_suffix(char *str, t_wildcard *psm);
+int			create_prefix(char *str, t_wildcard *psm);
+void		allocation_psm_middle(t_wildcard *psm, int middle_wildcard);
+void		create_psm_middle_suffix(char *str, t_wildcard *psm, int i);
+int			count_middle_wildcard(char *str);
+void		sort_argv(char **array);
+void		free_expand_variable(char *tmp, char *expanse, char *new_str);
 
 //wildcard_shearch
-int		ft_strnstr_for_wildcard(char *str,	char **middle, int len_str);
-void	free_wildcard(t_wildcard *psm, char **result, t_treenode *node);
-void	wildcard_redir(char **result, t_ctx *ctx, t_wildcard *psm, \
+int			ft_strnstr_for_wildcard(char *str,	char **middle, int len_str);
+void		free_wildcard(t_wildcard *psm, char **result, t_treenode *node);
+void		wildcard_redir(char **result, t_ctx *ctx, t_wildcard *psm,\
 											t_treenode *node);
-char	*result_wildcard(char **result, t_treenode *node, t_ctx *ctx, \
+char		*result_wildcard(char **result, t_treenode *node, t_ctx *ctx,\
 											t_wildcard *psm);
-char	*expand_wildcard(char *str, t_treenode *node, t_ctx *ctx, \
+char		*expand_wildcard(char *str, t_treenode *node, t_ctx *ctx,\
 											int wildcard_index);
-char	**found_match(DIR *dir, t_wildcard *psm, char **result);
+char		**found_match(DIR *dir, t_wildcard *psm, char **result);
 //wildcard_new_argv
-void	create_new_argv(char **result, t_treenode *node, \
+void		create_new_argv(char **result, t_treenode *node,\
 											t_wildcard *idx);
-char	**build_new_argv(t_treenode *node, char **result, t_wildcard *idx, \
+char		**build_new_argv(t_treenode *node, char **result, t_wildcard *idx,\
 												char **new_argv);
-int		len_array(char **result);
+int			len_array(char **result);
 
 #endif
