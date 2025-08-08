72b50b3c076f96506ba2108d8336273614ac591d
Zephyre <laurelineduf@proton.me>
2025-08-08 14:43:07 +0200

    fix probleme with heredoc without cmd, and fix << EOF echo hello > test1

--- execution/execute_redirection.c
+++ execution/execute_redirection.c
@@ -6,7 +6,7 @@
 /*   By: npederen <npederen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/25 11:00:27 by lduflot           #+#    #+#             */
-/*   Updated: 2025/08/07 16:16:24 by lduflot          ###   ########.fr       */
+/*   Updated: 2025/08/08 14:40:00 by lduflot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,33 +15,125 @@
 int	redir_heredoc(t_treenode *node, t_ctx *ctx);
 /*Applique toutes les redirs de l'AST,
 Exe la cmd, et save les entrées/sortie.*/
+char **copy_argv_hd_in_newcmd(char **argv);
+char **recup_all_argv_hd(t_treenode *node)
+{
+  char    **result;
+  char    **curent;
+  char    **merged;
+  int     len_res;
+  int     len_cur;
+  int     i;
+  int     j;
+
+  result = NULL;
+  curent = NULL;
+  merged = NULL;
+  len_res = 0;
+  len_cur = 0;
+  i = 0;
+  j = 0;
+  if (node->left && node->left->type == HERE_DOCUMENT)
+		result = recup_all_argv_hd(node->left);
+  curent = copy_argv_hd_in_newcmd(node->right->argv);
+	if (!curent)
+		return (result);
+  if (result)
+  {
+		while (result[len_res])
+			len_res++;
+  }
+  while (curent[len_cur]) 
+		len_cur++;
+  merged = malloc(sizeof(char*) * (len_res + len_cur + 1));
+  if (!merged)
+		return (result);
+  while (j < len_res)
+	{
+		merged[i] = result[j];
+		i++;
+		j++;
+	}
+  j = 0;
+  while (j < len_cur)
+	{
+  	merged[i] = curent[j];
+		i++;
+		j++;
+	}
+  merged[i] = NULL;
+  free(result);
+  free(curent);
+  return (merged);
+}
+
+char **copy_argv_hd_in_newcmd(char **argv)
+{
+	char	**new_argv;
+	int		i;
+	int		j;
+
+	i = 0;
+	j = 1;
+	if (!argv || !argv[1])
+		return (NULL);
+	while (argv[j] != NULL)
+		j++;
+	new_argv = malloc(sizeof(char *) * (j + 1));
+	if (!new_argv)
+		return (NULL);
+	j = 1;
+	while (argv[j])
+	{
+		new_argv[i] = ft_strdup(argv[j]);
+		i++;
+		j++;
+	}
+	new_argv[i] = NULL;
+	return (new_argv);
+}
 
 int	execute_redirection_chain(t_treenode *node, char *line, t_ctx *ctx)
 {
-	int			saved_stdin;
-	int			saved_stdout;
-	int			status;
+	int	saved_stdin;
+	int	saved_stdout;
+	int	status;
 	t_treenode	*cmd;
+	char	**tmp_argv;
 
 	saved_stdin = dup(STDIN_FILENO);
 	saved_stdout = dup(STDOUT_FILENO);
-	new_argv_cmd(node);
+	cmd = find_command_node(node);
+	if (cmd)
+		new_argv_cmd(node);
 	status = apply_redirections(node, line, ctx);
 	if (status != 0)
 	{
-		ctx->exit_code = 1;
 		dup_and_close(saved_stdin, saved_stdout);
-		return (1);
+		return (ctx->exit_code = 1);
 	}
-	cmd = find_command_node(node);
-	if (!cmd || !cmd->argv || !cmd->argv[0])
+	if (cmd && cmd->argv && cmd->argv[0]) 
 	{
+		status = execute_simple_command(cmd, line, ctx);
 		dup_and_close(saved_stdin, saved_stdout);
-		return (0);
-	}
-	status = execute_node(cmd, line, ctx);
+		return (status);
+  }
+  tmp_argv = recup_all_argv_hd(node);
+  if (tmp_argv && tmp_argv[0]) 
+	{
+      t_treenode cmd_test;
+			cmd_test.type  = WORD;
+			cmd_test.argv  = tmp_argv;
+			cmd_test.str   = tmp_argv[0];
+			cmd_test.left  = NULL;
+			cmd_test.right = NULL;
+      status = execute_node(&cmd_test, line, ctx);
+      free_split(tmp_argv);
+      dup_and_close(saved_stdin, saved_stdout);
+      return (status);
+  }
 	dup_and_close(saved_stdin, saved_stdout);
-	return (status);
+	return (0);
 }
 
 /*Parcours les node de redirs de manière récursive de gauche à droite*/
--- execution/execution.c
+++ execution/execution.c
@@ -6,7 +6,7 @@
 /*   By: npederen <npederen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/25 10:55:01 by lduflot           #+#    #+#             */
-/*   Updated: 2025/08/07 20:07:51 by lduflot          ###   ########.fr       */
+/*   Updated: 2025/08/08 14:23:14 by lduflot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,89 +31,27 @@ void	execute_tree(t_treenode *tree, char *line, t_ctx	*ctx)
 	ctx->exit_code = execute_node(tree, line, ctx);
 }
 
-char **copy_argv_hd_in_newcmd(char **argv)
-{
-	char	**new_argv;
-	int		i;
-	int		j;
-
-	i = 0;
-	j = 1;
-	if (!argv || !argv[1])
-		return (NULL);
-	while (argv[j] != NULL)
-		j++;
-	new_argv = malloc(sizeof(char *) * (j + 1));
-	if (!new_argv)
-		return (NULL);
-	j = 1;
-	while (argv[j])
-	{
-		new_argv[i] = ft_strdup(argv[j]);
-		i++;
-		j++;
-	}
-	new_argv[i] = NULL;
-//	printf("new argv = %s ;  %s", new_argv[0], new_argv[1]);
-	return (new_argv);
-}
-
 int    execute_node(t_treenode *node, char *line, t_ctx *ctx)
 {
- if (!node)
-     return (1);
- if (node->type == WORD || node->type == PIPE)
-     return (execute_node_simple(node, line, ctx));
- else if (node->type == LOGICAL_AND || node->type == LOGICAL_OR
-     || node->type == SEMICOLON)
-     return (execute_node_logical(node, line, ctx));
- else if (node->type == HERE_DOCUMENT
-     || node->type == INPUT_REDIRECTION
-     || node->type == OUTPUT_REDIRECTION
-     || node->type == APPEND_OUTPUT_REDIRECTION
-     || node->type == SUBSHELL)
-     {
-         if (node->type == HERE_DOCUMENT && node->right && ft_strcmp(node->right->str, "") == 0 && !node->left)
-         {
-             execute_node_redir(node, line, ctx);
-             return(execute_node(node->right, line, ctx));
-         }
-				else if (
-			(node->type == HERE_DOCUMENT
-			&& node->right 
-			&& node->right->argv 
-			&& node->right->argv[1]) 
-			||(node->type == HERE_DOCUMENT
-			&& node->left
-			&& node->left->type == HERE_DOCUMENT))
-				{
-					if (node->left && node->type == HERE_DOCUMENT)
-						node = node->left;
-					t_treenode *cmd_test = NULL;
-					cmd_test = malloc(sizeof(t_treenode));
-					if (!cmd_test)
-						return (ctx->exit_code = 1);
-					cmd_test->type = WORD;
-					//cmd_test->argv = save_cmd;
-					cmd_test->argv = copy_argv_hd_in_newcmd(node->right->argv);
-				if (!cmd_test->argv)
-				{
-					free(cmd_test);
-					return (ctx->exit_code = 0);
-				}
-					cmd_test->str = ft_strdup(cmd_test->argv[0]);
-					//	printf("cmd = %s , %s ", cmd_test->argv[0], cmd_test->argv[1]);
-					cmd_test->left = node;
-					int exit_code = 0;
-					exit_code = execute_node(cmd_test, line, ctx);
-					free(cmd_test->str);
-					free(cmd_test);
-					return (exit_code);
-			}
-			return (execute_node_redir(node, line, ctx));
-   	  }
-		 else
-    	 return (1);
+	if (!node)
+		return (1);
+	if (node->type == WORD || node->type == PIPE)
+		return (execute_node_simple(node, line, ctx));
+	else if (node->type == LOGICAL_AND || node->type == LOGICAL_OR
+			|| node->type == SEMICOLON)
+		return (execute_node_logical(node, line, ctx));
+	else if (node->type == HERE_DOCUMENT
+			|| node->type == INPUT_REDIRECTION
+			|| node->type == OUTPUT_REDIRECTION
+			|| node->type == APPEND_OUTPUT_REDIRECTION)
+  	{
+			expanse_ast(node, ctx);
+			return (execute_redirection_chain(node, line, ctx));
+		}
+	else if (node->type == SUBSHELL)
+		return (execute_subshell_node(node, line, ctx));
+	else
+		return (1);
 }
 
 int	execute_node_simple(t_treenode *node, char *line, t_ctx *ctx)
@@ -152,19 +90,4 @@ int	execute_node_logical(t_treenode *node, char *line, t_ctx *ctx)
 	}
 	ctx->exit_code = 1;
 	return (ctx->exit_code);
-}
-
-int	execute_node_redir(t_treenode *node, char *line, t_ctx *ctx)
-{
-	if (node->type == INPUT_REDIRECTION
-		|| node->type == OUTPUT_REDIRECTION
-		|| node->type == APPEND_OUTPUT_REDIRECTION
-		|| node->type == HERE_DOCUMENT)
-	{
-		expanse_ast(node, ctx);
-		return (execute_redirection_chain(node, line, ctx));
-	}
-	else if (node->type == SUBSHELL)
-		return (execute_subshell_node(node, line, ctx));
-	return (1);
 }
