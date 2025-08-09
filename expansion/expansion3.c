572a0ccb80fc8ab22feaf4c57660e253a71c48aa
Zephyre <laurelineduf@proton.me>
2025-08-09 20:18:13 +0200

    fix invalid read of size

--- expansion/expansion3.c
+++ expansion/expansion3.c
@@ -6,15 +6,17 @@
 /*   By: lduflot <lduflot@student.42perpignan.fr>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/09 17:43:35 by lduflot           #+#    #+#             */
-/*   Updated: 2025/08/09 18:01:03 by lduflot          ###   ########.fr       */
+/*   Updated: 2025/08/09 20:16:55 by lduflot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "expansion.h"
 
+//*i>0 (et pour l'invalid read of size lié au str[*i-1]
+//Ces deux fonction gères : "$" ou '$' et $4HOME = HOME ou $'HOME' = HOME
 int	var_with_number_after_dollar(int *j, char *str, int *i)
 {
-	if ((str[*i - 1] == '\'' || str[*i - 1] == '"') && (str[*i] == '$')
+	if (*i > 0 && str[*i] && (str[*i - 1] == '\'' || str[*i - 1] == '"') && (str[*i] == '$')
 		&& (str[*i + 1] == '\'' || str[*i + 1] == '"'))
 		return (0);
 	if (str[*i] == '$'
@@ -31,7 +33,7 @@ int	var_with_number_after_dollar(int *j, char *str, int *i)
 
 int	var_with_quote_after_dollar(int *j, char **result, char *str, int *i)
 {
-	if ((str[*i - 1] == '\'' || str[*i - 1] == '"') && (str[*i] == '$')
+	if (*i > 0 && str[*i] && (str[*i - 1] == '\'' || str[*i - 1] == '"') && (str[*i] == '$')
 		&& (str[*i + 1] == '\'' || str[*i + 1] == '"'))
 		return (0);
 	if (str[*i] == '$'
